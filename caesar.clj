95148d1920ac61444f5f475b18d4527ebbbc293d
jarbouvic <jarbouvic@gmail.com>
2014-11-13 17:06:01 +0700

    Update caesar.clj

--- caesar.clj
+++ caesar.clj
@@ -4,16 +4,10 @@
 ;; Find the char equivalent to e by looking for the char with
 ;; max occurence in a text. Then permutate all chars
 
-;; Contract: char-occurence : string -> sequence of number
-;; Purpose:  Find number of occurence of all characters in a text
-;; Example:  (char-occurence "wonderfultext") =>
-;; => [0 0 0 1 2 1 0 0 0 0 0 1 0 1 1 0 0 1 0 2 1 0 1 1 0 0]
-(defn char-occurence
-  [text]
-  (reduce []))
-
+;; Latin alphabet of 26 character with occurence null
 (def alphabet (repeat 26 0))
 
+;; Example text
 (def text
 "In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.The encryption step performed by a Caesar cipher is often incorporated as part of more complex schemes, such as the Vigenère cipher, and still has modern application in the ROT13 system. As with all single alphabet substitution ciphers, the Caesar cipher is easily broken and in modern practice offers essentially no communication security.")
 
